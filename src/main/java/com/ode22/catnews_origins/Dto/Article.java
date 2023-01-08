c3a589917cb10fcdf42e16e4d841634638969c83
Paco <patrick.cosic@hotmail.com>
2023-01-08 13:16:02 +0100

    removes unused article-properties

--- src/main/java/com/ode22/catnews_origins/Dto/Article.java
+++ src/main/java/com/ode22/catnews_origins/Dto/Article.java
@@ -10,26 +10,14 @@ import java.util.Map;
 public class Article {
     @JsonProperty("TITEL")
     String titel;
-    @JsonProperty("UTL")
-    String utl;
     @JsonProperty("INHALT")
     String inhalt;
-    @JsonProperty("RUECKFRAGEHINWEIS")
-    String rueckfragehinweis;
     @JsonProperty("DATUM")
     String datum;
     @JsonProperty("ZEIT")
     String zeit;
-    @JsonProperty("ZEITSTEMPEL")
-    String zeitstempel;
-    @JsonProperty("ANHANG")
-    List<Object> anhang;
     @JsonProperty("WEBLINK")
     String weblink;
-    @JsonProperty("EMITTENT")
-    String emittent;
-    @JsonProperty("EMITTENTID")
-    String emittentid;
 
     public String getTitel() {
         return titel;
@@ -39,28 +27,12 @@ public class Article {
         this.titel = titel;
     }
 
-    public String getUtl() {
-        return utl;
-    }
-
-    public void setUtl(String utl) {
-        this.utl = utl;
-    }
-
     public String getInhalt() {
         return inhalt;
     }
 
     public void setInhalt(String inhalt) {
         this.inhalt = inhalt;
-    }
-
-    public String getRueckfragehinweis() {
-        return rueckfragehinweis;
-    }
-
-    public void setRueckfragehinweis(String rueckfragehinweis) {
-        this.rueckfragehinweis = rueckfragehinweis;
     }
 
     public String getDatum() {
@@ -79,43 +51,11 @@ public class Article {
         this.zeit = zeit;
     }
 
-    public String getZeitstempel() {
-        return zeitstempel;
-    }
-
-    public void setZeitstempel(String zeitstempel) {
-        this.zeitstempel = zeitstempel;
-    }
-
-    public List<Object> getAnhang() {
-        return anhang;
-    }
-
-    public void setAnhang(List<Object> anhang) {
-        this.anhang = anhang;
-    }
-
     public String getWeblink() {
         return weblink;
     }
 
     public void setWeblink(String weblink) {
         this.weblink = weblink;
-    }
-
-    public String getEmittent() {
-        return emittent;
-    }
-
-    public void setEmittent(String emittent) {
-        this.emittent = emittent;
-    }
-
-    public String getEmittentid() {
-        return emittentid;
-    }
-
-    public void setEmittentid(String emittentid) {
-        this.emittentid = emittentid;
     }
 }
